d4e95af7313017fbbc61fa2349858f689efb77da
Oblitus <piotr5jurczak@gmail.com>
2023-03-29 00:11:14 +0200

    rustPlayground

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::net::TcpListener;
+
+
+use sysinfo::{System, SystemExt, NetworkExt};
+fn main() {
+    let  sys = System::new_all();
+    for(interface_nam,data) in sys.networks(){
+        println!("{}: {} (mac)",interface_nam,data.mac_address());
+        
+    }
+    // let listener = TcpListener::bind("127.0.0.1:5010").unwrap();
+
+    // for _stream in listener.incoming(){
+    //     let stream = _stream.unwrap();
+        
+    //     println!("connected {}", stream.ttl().unwrap());
+    // }
+}
