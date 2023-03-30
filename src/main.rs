e44c3bb320b1e33079cd986b39e12f18909ae4b1
Oblitus <piotr5jurczak@gmail.com>
2023-03-30 15:52:32 +0200

    Ustawienie pomijania wirtualnych kart sieciowych

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::net::TcpListener;
-
-
+//use std::net::TcpListener;
 use sysinfo::{System, SystemExt, NetworkExt};
+
 fn main() {
     let  sys = System::new_all();
     for(interface_nam,data) in sys.networks(){
-        println!("{}: {} (mac)",interface_nam,data.mac_address());
-        
+        if data.mac_address().to_string() != "00:00:00:00:00:00" {
+            println!("{}: {} (mac)",interface_nam,data.mac_address());
+        }
+
     }
+    //
     // let listener = TcpListener::bind("127.0.0.1:5010").unwrap();
 
     // for _stream in listener.incoming(){
